2562310378f6df585cc685926458fec49d85815d
Adao Pedro <adaopedro.pro@gmail.com>
2026-06-11 21:49:39 +0100

    adiciona configuracoes de CORS

--- public/index.php
+++ public/index.php
@@ -6,6 +6,16 @@ require "../vendor/autoload.php";
 
 $r = new Router;
 
+// Configuracoes de CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+// Responde ao Preflight e para aqui
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
+
 $r->get("/api/clientes", "\App\Controllers\ClienteController@getAll");
 $r->get("/api/clientes/{id}", "\App\Controllers\ClienteController@getById");
 $r->post("/api/clientes", "\App\Controllers\ClienteController@create");
